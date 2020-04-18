8712881aaf2cd5cac8be311509c9a274483466a3
Emma <enabojohn@gmail.com>
2020-04-18 12:46:25 +0300

    Generated googleClientID and googleClientSecrete with Google Cloud Platform and configured redirect URLs callback

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI:'mongodb+srv://emma:ehelisemmy@cluster0-cde8d.mongodb.net/test?retryWrites=true&w=majority',
+    googleClientID:'85797508044-qgkb6t64n2rbirb8cffhlsi1jrr8r2pe.apps.googleusercontent.com',
+    googleClientSecret:'z3dqMlegt5XeA7l8Jv8_iVjs',
+}; 
