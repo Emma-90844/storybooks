fefeaead57e0cfff32172bb81f60b133886419ee
Emma <enabojohn@gmail.com>
2020-04-18 12:35:44 +0300

    Required auth from routes/auth set it to use /auth

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
+//Passport Config
+require('./config/passport')(passport);
+
+//Load Routes
+const auth = require('./routes/auth');
+
 
 const app = express();
 
@@ -8,8 +16,32 @@ app.get('/', (req, res) => {
 });
 
 
+
+//Routes Use
+app.use('/auth', auth); 
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Server started at port ${port}`);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
