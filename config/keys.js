4e12e1b2719cdfbe6b84e4966a4b1c0af4dec2d1
Emma <enabojohn@gmail.com>
2020-04-20 10:23:30 +0300

    Changed google client Id and the client secrete key

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
     mongoURI:'mongodb+srv://emma:ehelisemmy@cluster0-cde8d.mongodb.net/test?retryWrites=true&w=majority',
-    googleClientID:'85797508044-qgkb6t64n2rbirb8cffhlsi1jrr8r2pe.apps.googleusercontent.com',
-    googleClientSecret:'z3dqMlegt5XeA7l8Jv8_iVjs',
+    googleClientID:'124985823550-figtffhbe1bkh32vn2pi5ursfjruhm14.apps.googleusercontent.com',
+    googleClientSecret:'v-Ne06cU4L-jY2jZx403Yb8E',
 }; 
 
