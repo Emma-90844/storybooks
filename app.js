57e3cda0cdca6c3edb2c72cf740cfebb1f3f0c3f
Emma <enabojohn@gmail.com>
2020-05-07 14:52:55 +0300

    worked on log in link

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
+const path = require('path');
 const express = require('express');
+const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const passport = require('passport');
 const session = require('express-session');
+
 //Load User Model
 require('./models/User');
+
 // Passport Config
 require('./config/passport')(passport);
 
 // Load Routes
+const index = require('./routes/index');
 const auth = require('./routes/auth');
 
 // Load Keys
@@ -24,13 +29,20 @@ mongoose.Promise = global.Promise;
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('It Works!');
-});
+
+
+//Handlebars Middleware
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main',
+}));
+app.set('view engine', 'handlebars');
+
+
+//static folser configuration
+app.use(express.static(path.join(__dirname + '/public')));
 
 //Adding Session
 app.use(cookieParser());
-
 //Adding session
 app.use(session({
   secret: 'secret',
@@ -44,11 +56,12 @@ app.use(passport.session());
 
 //Set global variables
 app.use((req, res, next) => {
-  res.locals.user = req.user || null
+  res.locals.user = req.user || null;
   next();
-})
+});
 
 // Use Routes
+app.use('/', index);
 app.use('/auth', auth);
 
 //Listening Port
@@ -56,3 +69,4 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
+
